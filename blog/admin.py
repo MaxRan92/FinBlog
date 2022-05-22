8c4745ff4b5b0b621b66d3061b8c3fb5a5d7b1c2
Massimo Ranalli <ranallimax@gmail.com>
2022-05-22 08:59:03 +0000

    add summernote model to content

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    # add the post models to the admin panel
+    # apply summernote to the content text field
+
+    summernote_fields = ('content')
+
