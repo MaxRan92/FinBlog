43329e2c71a93102de39ef278b75c7a3ebca0c9b
Massimo Ranalli <ranallimax@gmail.com>
2022-05-22 09:06:58 +0000

    Add filter, listdisplay, search functionalities to PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,11 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
     # add the post models to the admin panel
     # apply summernote to the content text field
+    # prepopulate slug with title, add filter, listdisplay, search functionality
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
