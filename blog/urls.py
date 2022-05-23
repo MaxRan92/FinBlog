b49cb9e6aeff3049f037cfb428f1b75067674041
Massimo Ranalli <ranallimax@gmail.com>
2022-05-23 22:19:02 +0000

    create asset_detail general page and its view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.AssetList.as_view(), name='home')
+    path('', views.AssetList.as_view(), name='home'),
+    path('<slug:slug>/', views.AssetDetail.as_view(), name='asset_detail'),
 ]
