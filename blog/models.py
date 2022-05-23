f4ba1d3deb9c6591bb08b60cf6d95959e384c276
Massimo Ranalli <ranallimax@gmail.com>
2022-05-23 22:02:24 +0000

    update Post model fields: add ticker, delete likes

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    ticker = models.CharField(max_length=6, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
@@ -16,7 +17,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    
 
     class Meta:
         ordering = ["-created_on"]
